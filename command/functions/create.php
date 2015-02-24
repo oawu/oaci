95a74f8499219173e2e3a01e979bcd335923cc64
OA Wu <comdan66@gmail.com>
2015-02-24 12:51:27 +0800

    Add create migration, and modify controller.

--- command/functions/create.php
+++ command/functions/create.php
@@ -31,27 +31,20 @@ if (!function_exists ('create_controller')) {
       @mkdir ($name_view_path = $contents_path . $name . '/', 0777, true);
       umask ($oldmask);
 
-      if (is_writable ($name_view_path)) {
-        if ($methods) {
-          foreach ($methods as $method) {
-            $oldmask = umask (0);
-            @mkdir ($name_view_path . $method . '/', 0777, true);
-            umask ($oldmask);
+      if (!is_writable ($name_view_path))
+        return @unlink ($name_controller_path);
 
-            if (is_writable ($name_view_path . $method . '/')) {
-              $files = array ('content.css', 'content.scss', 'content.js', 'content.php');
+      array_map (function ($method) use ($name_view_path) {
+        $oldmask = umask (0);
+        @mkdir ($name_view_path . $method . '/', 0777, true);
+        umask ($oldmask);
 
-              if ($files) {
-                foreach ($files as $file) {
-                  write_file ($name_view_path . $method . '/' . $file, load_view ('templates/' . $file));
-                }
-              }
-            }
-          }
-        }
-      } else {
-        @unlink ($name_controller_path);
-      }
+        if (!is_writable ($name_view_path . $method . '/'))
+          return null;
+
+        $files = array ('content.css', 'content.scss', 'content.js', 'content.php');
+        array_map (function ($file) use ($name_view_path, $method) { write_file ($name_view_path . $method . '/' . $file, load_view ('templates/' . $file)); }, $files);
+      }, $methods);
     } else {
       console_log ("新增 controller 失敗!");
     }
@@ -78,7 +71,9 @@ if (!function_exists ('create_model')) {
       console_log ("Uploader 無法有寫入的權限!");
 
     $columns = array_filter (array_map (function ($column) use ($name, $uploaders_path, $uploaders) {
+      $column = strtolower ($column);
       $uploader = ucfirst (camelize ($name)) . ucfirst ($column) . 'Uploader';
+
       if (!in_array ($uploader, $uploaders) && write_file ($uploaders_path . $uploader . EXT, "<?php" . load_view ('templates/uploader.php', array ('name' => $uploader))))
         return $column;
       return null;
@@ -87,5 +82,29 @@ if (!function_exists ('create_model')) {
     $date = "<?php" . load_view ('templates/model.php', array ('name' => $name, 'columns' => $columns));
     if (!write_file ($models_path . ucfirst (camelize ($name)) . EXT, $date))
       array_map (function ($column) use ($name, $uploaders_path) { @unlink ($uploaders_path . ucfirst (camelize ($name)) . ucfirst ($column) . 'Uploader' . EXT); }, $columns);
+  }
+}
+
+if (!function_exists ('create_migration')) {
+  function create_migration ($name, $action) {
+    $action = ($action == '-e') || ($action == '-edit') ? 'edit' : 'add';
+
+    $migrations_path = FCPATH . 'application/migrations/';
+    $migrations = array_filter (array_map (function ($t) { return '.' . pathinfo ($t, PATHINFO_EXTENSION) == EXT ? basename ($t, EXT) : null; }, directory_map ($migrations_path, 1)));
+
+    if (!is_writable ($migrations_path))
+      console_log ("無法有寫入的權限!");
+
+    $count = sprintf ("%03d", max (array_map (function ($migration) { return substr ($migration, 0, strpos ($migration, '_')); }, $migrations)) + 1);
+
+    if ($migrations && in_array ($file_name = $count . '_' . $action . '_' . pluralize ($name), $migrations))
+      console_log ("名稱錯誤!");
+    else
+      $file_name .= EXT;
+
+    $date = "<?php" . load_view ('templates/migration.php', array ('name' => $name, 'action' => $action));
+
+    if (!write_file ($migrations_path . $file_name, $date))
+      console_log ("寫檔失敗!");
   }
 }
