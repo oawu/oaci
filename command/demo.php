b1a051c20742806303d6490b7d3843098eaa93ea
OA Wu <comdan66@gmail.com>
2015-02-27 00:02:14 +0800

    Modify demo.

--- command/demo.php
+++ command/demo.php
@@ -16,70 +16,66 @@
 // Event
 // Attendee
 
-  // $temp_path = FCPATH . 'command/templates/demo/event/';
-  // $name = 'event';
-  // $action = 'add';
-  // create_migration ($temp_path, $name, $action);
+  $temp_path = FCPATH . 'command/templates/demo/event/';
+  $name = 'event';
+  $action = 'add';
+  create_migration ($temp_path, $name, $action);
 
-  // $temp_path = FCPATH . 'command/templates/demo/attendee/';
-  // $name = 'attendee';
-  // $action = 'add';
-  // create_migration ($temp_path, $name, $action);
+  $temp_path = FCPATH . 'command/templates/demo/attendee/';
+  $name = 'attendee';
+  $action = 'add';
+  create_migration ($temp_path, $name, $action);
 
-  // $temp_path = FCPATH . 'command/templates/demo/tag/';
-  // $name = 'tag';
-  // $action = 'add';
-  // create_migration ($temp_path, $name, $action);
+  $temp_path = FCPATH . 'command/templates/demo/tag/';
+  $name = 'tag';
+  $action = 'add';
+  create_migration ($temp_path, $name, $action);
 
-  // $temp_path = FCPATH . 'command/templates/demo/tag_event_map/';
-  // $name = 'tag_event_map';
-  // $action = 'add';
-  // create_migration ($temp_path, $name, $action);
-
-
-
-  // $temp_path = FCPATH . 'command/templates/demo/event/';
-  // $name = 'event';
-  // $action = array ('cover');
-  // create_model ($temp_path, $name, $action);
-
-  // $temp_path = FCPATH . 'command/templates/demo/attendee/';
-  // $name = 'attendee';
-  // $action = array ();
-  // create_model ($temp_path, $name, $action);
-
-  // $temp_path = FCPATH . 'command/templates/demo/tag/';
-  // $name = 'tag';
-  // $action = array ();
-  // create_model ($temp_path, $name, $action);
-
-  // $temp_path = FCPATH . 'command/templates/demo/tag_event_map/';
-  // $name = 'tag_event_map';
-  // $action = array ();
-  // create_model ($temp_path, $name, $action);
+  $temp_path = FCPATH . 'command/templates/demo/tag_event_map/';
+  $name = 'tag_event_map';
+  $action = 'add';
+  create_migration ($temp_path, $name, $action);
 
 
-// ------------------------------------------------------------------------------------------------------------
 
+  $temp_path = FCPATH . 'command/templates/demo/event/';
+  $name = 'event';
+  $action = array ('cover');
+  create_model ($temp_path, $name, $action);
 
-  // $temp_path = FCPATH . 'command/templates/demo/cell/';
-  // $name = 'demo';
-  // $action = array ('main_menu');
-  // create_cell ($temp_path, $name, $action);
+  $temp_path = FCPATH . 'command/templates/demo/attendee/';
+  $name = 'attendee';
+  $action = array ();
+  create_model ($temp_path, $name, $action);
+
+  $temp_path = FCPATH . 'command/templates/demo/tag/';
+  $name = 'tag';
+  $action = array ();
+  create_model ($temp_path, $name, $action);
+
+  $temp_path = FCPATH . 'command/templates/demo/tag_event_map/';
+  $name = 'tag_event_map';
+  $action = array ();
+  create_model ($temp_path, $name, $action);
 
 
 // ------------------------------------------------------------------------------------------------------------
 
 
-  // $temp_path = FCPATH . 'command/templates/demo/event/';
-  // $name = 'event';
-  // $action = 'site';
-  // $results = create_controller ($temp_path, $name, $action, array ('index', 'show', 'add', 'create', 'edit', 'update', 'destroy'));
+  $temp_path = FCPATH . 'command/templates/demo/cell/';
+  $name = 'demo';
+  $action = array ('main_menu');
+  create_cell ($temp_path, $name, $action);
 
-  // $temp_path = FCPATH . 'command/templates/demo/event/cell/';
-  // $name = 'event';
-  // $action = array ('main_menu');
-  // create_cell ($temp_path, $name, $action);
+
+// ------------------------------------------------------------------------------------------------------------
+
+
+  $temp_path = FCPATH . 'command/templates/demo/event/';
+  $name = 'event';
+  $action = 'site';
+  $results = create_controller ($temp_path, $name, $action, array ('index', 'show', 'add', 'create', 'edit', 'update', 'destroy'));
+
 
 
 
