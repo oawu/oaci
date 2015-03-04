5ac098b551063a89f0cc5dbb2ca9b1256d2ce33f
OA Wu <comdan66@gmail.com>
2015-03-04 09:52:19 +0800

    Create files.

---
+++ command/migration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 OA Wu Design
+ */
+
+  //       file       version
+  // ==========================================
+  // php   migration  [0 | 1 | 2...]
+
+  include 'base.php';
+
+  $file    = array_shift ($argv);
+  $version = array_shift ($argv);
+
