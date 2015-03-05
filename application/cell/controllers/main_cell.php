488338bcb3c5baf10d880c4570e6e3c5c581b396
OA Wu <comdan66@gmail.com>
2015-03-05 22:10:36 +0800

    Clean main controller.

--- application/cell/controllers/main_cell.php
+++ application/cell/controllers/main_cell.php
@@ -7,21 +7,9 @@
 class Main_cell extends Cell_Controller {
 
   public function _cache_index () {
-    return array ('time' => 5, 'key' => array ('user_id' . 5, '123'));
+    return array ('time' => 60 * 60, 'key' => null);
   }
   public function index () {
     return $this->load_view ();
-  }
-  public function _cache_type ($a = 0) {
-    return array ('time' => 60 * 60, 'key' => $a ? array ('user_id' . $a) : null);
-  }
-  public function type ($a = 0) {
-    return 'sss';
-  }
-  public function _cache_test () {
-    return array ('time' => 60 * 60, 'key' => array ());
-  }
-  public function test () {
-    return 'wwww';
   }
 }
