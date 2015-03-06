049c650f9f2950a4e92ed3a4f60684a0a75a0834
OA Wu <comdan66@gmail.com>
2015-03-06 10:23:35 +0800

    Fix bug.

--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -1,6 +1,1 @@
-
 <?php echo render_cell ('main_cell', 'index');?>
-<?php echo render_cell ('main_cell', 'type', 1);?>
-<?php echo render_cell ('main_cell', 'type', 2);?>
-<?php echo render_cell ('main_cell', 'type');?>
-<?php echo render_cell ('main_cell', 'test');?>
