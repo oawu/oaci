e8f71d923a9c186e4a0414e5e8d3cc826e512cc4
OA Wu <comdan66@gmail.com>
2015-03-17 15:58:21 +0800

    Temp.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -13,6 +13,11 @@ class Main extends Site_controller {
 
   public function index () {
     $this->add_js (base_url ('resource', 'javascript', 'jQueryMousewheel_v3.1.12', 'jquery.mousewheel.min.js'))
+         ->add_hidden (array ('id' => 'get_units_url', 'value' => base_url ($this->get_class (), 'get_units')))
          ->load_view (null);
+  }
+
+  public function get_units () {
+    $this->output_json (array ('next_id' => 0, 'units' => 'dasd'));
   }
 }
--- application/views/content/site/main/index/content.js
+++ application/views/content/site/main/index/content.js
@@ -5,16 +5,46 @@
 
 $(function () {
   var left = 0;
+  var d4_unit_count = 2;
   var d4_unit_width = 1000;
+
+
+  function load () {
+
+    $.ajax ({
+      url: $('#get_units_url').val (),
+      data: { next_id: 1 },
+      async: true, cache: false, dataType: 'json', type: 'POST',
+      beforeSend: function () {
+
+      }
+    })
+    .done (function (result) {
+console.info (result);
+    })
+    .fail (function (result) { ajaxError (result); })
+    .complete (function (result) { });
+  }
+
+
+  function move () {
+    $('.time_line').find ('.time_units').css ({'left': '+=' + left});
+  }
 
   $('.time_line').mousewheel (function (e) {
     left = Math.abs (e.deltaX) > Math.abs (e.deltaY) ? -e.deltaX : e.deltaY;
 
-    if ((parseFloat($(this).find ('.time_units').css ('left')) + left > 0) || ((parseFloat($(this).find ('.time_units').css ('left')) + left) < (0 - (d4_unit_width * (5 - 1)))))
+    if (parseFloat($(this).find ('.time_units').css ('left')) + left > 0)
       left = 0;
 
-    $(this).find ('.time_units').css ({'left': '+=' + left});
+    if ((parseFloat($(this).find ('.time_units').css ('left')) + left) < (0 - (d4_unit_width * (d4_unit_count - 1)))) {
+      load ();
+      left = 0;
+    }
+
+    move ();
     return false;
   });
 
+  move ();
 });
