4df0f5ea6b66f3a72fcb84433f9cd442e201b3f1
OA Wu <comdan66@gmail.com>
2015-03-17 11:39:54 +0800

    Finish clean cache model.

--- command/functions/clean.php
+++ command/functions/clean.php
@@ -18,6 +18,7 @@ if (!function_exists ('clean_cache_cell')) {
       return '清除 cell cache 失敗!';
   }
 }
+
 if (!function_exists ('clean_cache_static')) {
   function clean_cache_static ($CI) {
     $CI->load->library ('cfg');
@@ -29,6 +30,7 @@ if (!function_exists ('clean_cache_static')) {
       return '清除 static cache 失敗!';
   }
 }
+
 if (!function_exists ('clean_cache_file')) {
   function clean_cache_file ($CI) {
     $CI->load->library ('cfg');
@@ -37,11 +39,22 @@ if (!function_exists ('clean_cache_file')) {
     array_push ($cache, Cfg::system ('cache', 'file'));
     array_push ($cache, Cfg::system ('cache', 'output'));
     array_push ($cache, Cfg::system ('cache', 'config'));
+    array_push ($cache, Cfg::system ('cache', 'model'));
 
     if (array_filter (array_map (function ($t) { return directory_delete (FCPATH . implode (DIRECTORY_SEPARATOR, $t), false); }, $cache)))
       return '已經清除所有 file cache!';
     else
       return '清除 file cache 失敗!';
+  }
+}
+
+if (!function_exists ('clean_cache_model')) {
+  function clean_cache_model ($CI) {
+    $CI->load->library ('cfg');
+    if (ActiveRecord\Cache::flush ())
+      return '已經清除所有 model cache!';
+    else
+      return '清除 model cache 失敗!';
   }
 }
 
@@ -63,10 +76,15 @@ if (!function_exists ('clean_cache')) {
         array_push ($results, clean_cache_file ($CI));
         break;
 
+      case 'model':
+        array_push ($results, clean_cache_model ($CI));
+        break;
+
       default:
         array_push ($results, clean_cache_cell ($CI));
         array_push ($results, clean_cache_static ($CI));
         array_push ($results, clean_cache_file ($CI));
+        array_push ($results, clean_cache_model ($CI));
         break;
     }
 
