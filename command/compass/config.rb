8e7098e5bfb692e26ed425ef02886775b2418bae
OA Wu <comdan66@gmail.com>
2015-04-18 14:35:45 +0800

    Fix compass config file.

--- command/compass/config.rb
+++ command/compass/config.rb
@@ -9,6 +9,12 @@ sass_dir = "../../application"
 add_import_path = "./imports"
 additional_import_paths = ["./imports"]
 
+images_dir = "resource/image"
+images_path = "../../resource/image/"
+
+fonts_dir = "resource/font"
+fonts_path = "../../resource/font/"
+
 # images_dir = "images"
 # javascripts_dir = "javascripts"
 
