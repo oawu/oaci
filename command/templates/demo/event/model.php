975db4b22285cb9524dbd60daa5c09321d46fd51
OA Wu <comdan66@gmail.com>
2015-08-10 00:21:16 +0800

    Edit demo templates.

--- command/templates/demo/event/model.php
+++ command/templates/demo/event/model.php
@@ -5,14 +5,16 @@
  * @copyright   Copyright (c) 2015 OA Wu Design
  */
 
-class <?php echo ucfirst (camelize ($name));?> extends OaModel {
+class Event extends OaModel {
 
-  static $table_name = '<?php echo pluralize ($name);?>';
+  static $table_name = 'events';
 
+  // 建立 一對一 關聯變數
   static $has_one = array (
     array ('first_attendee', 'class_name' => 'Attendee', 'order' => 'id ASC'),
   );
 
+  // 建立 一對多 關聯變數
   static $has_many = array (
     array ('tag_event_maps', 'class_name' => 'TagEventMap'),
 
@@ -20,26 +22,28 @@ class <?php echo ucfirst (camelize ($name));?> extends OaModel {
     array ('tags', 'class_name' => 'Tag', 'through' => 'tag_event_maps')
   );
 
+  // 建立 一對一 關聯變數
   static $belongs_to = array (
   );
 
   public function __construct ($attributes = array (), $guard_attributes = true, $instantiating_via_find = false, $new_record = true) {
     parent::__construct ($attributes, $guard_attributes, $instantiating_via_find, $new_record);
 
-<?php
-    if ($images) { ?>
-<?php foreach ($images as $image) { ?>
-    OrmImageUploader::bind ('<?php echo $image; ?>', '<?php echo ucfirst (camelize ($name)) . ucfirst ($image) . $image_uploader_class_suffix; ?>');
-<?php
-      }
-    } ?>
+    // 綁定處理圖片的欄位
+    OrmImageUploader::bind ('cover', 'EventCoverImageUploader');
+  }
 
-<?php
-    if ($files) { ?>
-<?php foreach ($files as $file) { ?>
-    OrmFileUploader::bind ('<?php echo $file; ?>', '<?php echo ucfirst (camelize ($name)) . ucfirst ($file) . $file_uploader_class_suffix; ?>');
-<?php
-      }
-    } ?>
+  // return bool
+  public function destroy () {
+    // 取出相對應的 tag_event_maps 資料，並且將其刪除
+    if ($old_tag_ids = column_array ($this->tag_event_maps, 'tag_id'))
+      TagEventMap::delete_all (array ('conditions' => array ('tag_id IN (?)', $old_tag_ids)));
+
+    // 取出相對應的 attendees 資料，並且將其刪除
+    if ($old_attendee_ids = column_array ($this->attendees, 'id'))
+      Attendee::delete_all (array ('conditions' => array ('id IN (?)', $old_attendee_ids)));
+
+    // 將圖片檔案刪除，然後將自身資料刪除，並且回傳成功或者失敗
+    return $this->cover->cleanAllFiles () && $this->delete ();
   }
 }
--- command/templates/demo/tag/model.php
+++ command/templates/demo/tag/model.php
@@ -5,39 +5,32 @@
  * @copyright   Copyright (c) 2015 OA Wu Design
  */
 
-class <?php echo ucfirst (camelize ($name));?> extends OaModel {
+class Tag extends OaModel {
 
-  static $table_name = '<?php echo pluralize ($name);?>';
+  static $table_name = 'tags';
 
+  // 建立 一對一 關聯變數
   static $has_one = array (
   );
 
+  // 建立 一對多 關聯變數
   static $has_many = array (
     array ('tag_event_maps', 'class_name' => 'TagEventMap'),
 
     array ('events', 'class_name' => 'Event', 'through' => 'tag_event_maps')
   );
 
+  // 建立 一對一 關聯變數
   static $belongs_to = array (
   );
 
   public function __construct ($attributes = array (), $guard_attributes = true, $instantiating_via_find = false, $new_record = true) {
     parent::__construct ($attributes, $guard_attributes, $instantiating_via_find, $new_record);
-
-<?php
-    if ($images) { ?>
-<?php foreach ($images as $image) { ?>
-    OrmImageUploader::bind ('<?php echo $image; ?>', '<?php echo ucfirst (camelize ($name)) . ucfirst ($image) . $image_uploader_class_suffix; ?>');
-<?php
-      }
-    } ?>
-
-<?php
-    if ($files) { ?>
-<?php foreach ($files as $file) { ?>
-    OrmFileUploader::bind ('<?php echo $file; ?>', '<?php echo ucfirst (camelize ($name)) . ucfirst ($file) . $file_uploader_class_suffix; ?>');
-<?php
-      }
-    } ?>
+  }
+  
+  // return bool
+  public function destroy () {
+    // 將自身資料刪除，並且回傳成功或者失敗
+    return $this->delete ();
   }
 }
