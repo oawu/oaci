f907b33c57e6f0ca8117206cefaff34c93d31a20
OA Wu <comdan66@gmail.com>
2015-12-14 16:58:06 +0800

    OrmImageUploader add compressor feature.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -8,10 +8,6 @@
 class Main extends Site_controller {
 
   public function index () {
-    $this->load->library ('CompressorIo');
-    CompressorIo::postAndDownload (array ('temp/805597940_566e39c62b4d7.png', 'temp/08.jpg'));
-    CompressorIo::postAndDownload ('temp/08.jpg');
-
     $this->load_view ();
   }
 }
