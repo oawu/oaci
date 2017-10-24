e34b234948ba34ba02d5cfc75525fd75bf38ab93
OA Wu <comdan66@gmail.com>
2017-10-24 10:22:03 +0800

    Fix code.

--- application/views/content/admin/ckeditor/image_browser/content.php
+++ application/views/content/admin/ckeditor/image_browser/content.php
@@ -9,9 +9,9 @@
 <div id='imgs' class='<?php echo $now . ($objs ? '' : ' e');?>'>
 <?php
   foreach ($objs as $obj) { ?>
-    <div data-url='<?php echo $obj->name->url ('400h');?>'>
+    <div data-url='<?php echo $obj->name->url ('h800');?>'>
       <div><img src='<?php echo $obj->user->avatar ();?>'></div>
-      <img src='<?php echo $obj->name->url ('400h');?>'>
+      <img src='<?php echo $obj->name->url ('h800');?>'>
       <time datetime='<?php echo $obj->created_at->format ('Y-m-d H:i:s');?>'><?php echo $obj->created_at->format ('Y-m-d H:i:s');?></span>
     </div>
   <?php
