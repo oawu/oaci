dae13fb1eb91f387bf1e0b1e0df3f2e30826feda
OA Wu <comdan66@gmail.com>
2017-12-27 23:28:38 +0800

    ⭐️ 加入 ORM - PHP ActiveRecord Website：http://www.phpactiverecord.org/ GitHub：https://github.com/jpfuentes2/php-activerecord
    ◎ Origin Repository：ci316(https://github.com/comdan66/ci316)
    ◎ Origin Commit：71a1eb3b155206a0eece959fe4eb5be8e7a40a2d [71a1eb3]

---
+++ application/core/MY_Model.php
@@ -0,0 +1,9 @@
+<?php defined ('BASEPATH') OR exit ('No direct script access allowed');
+
+class Model extends ActiveRecord\Model {
+
+  public function __construct () {
+    parent::__construct ();
+
+  }
+}
