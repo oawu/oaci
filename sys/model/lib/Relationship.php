2a7dd04cc88f3c54ab0db95cb64b61adf5d58a01
OA Wu <comdan66@gmail.com>
2017-12-27 23:43:22 +0800

    📝 ORM include feature
    ◎ Origin Repository：ci316(https://github.com/comdan66/ci316)
    ◎ Origin Commit：78c697d6b0148e534c52874f37d9b0bb6718866c [78c697d]

--- sys/model/lib/Relationship.php
+++ sys/model/lib/Relationship.php
@@ -64,7 +64,7 @@ abstract class AbstractRelationship implements InterfaceRelationship
 	 *
 	 * @var array
 	 */
-	static protected $valid_association_options = array('class_name', 'class', 'foreign_key', 'conditions', 'select', 'readonly', 'namespace');
+	static protected $valid_association_options = array('include', 'class_name', 'class', 'foreign_key', 'conditions', 'select', 'readonly', 'namespace');
 
 	/**
 	 * Constructs a relationship.
@@ -136,6 +136,8 @@ abstract class AbstractRelationship implements InterfaceRelationship
 
 		foreach ($attributes as $column => $value)
 			$values[] = $value[$inflector->variablize($model_values_key)];
+
+		// $values = array_unique ($values);
 
 		$values = array($values);
 		$conditions = SQLBuilder::create_conditions_from_underscored_string($table->conn,$query_key,$values);
